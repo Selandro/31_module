e21718ec95cb5bfccc39c2290d3ee8701bbbc64b
selandro <vadim-selandro@yandex.ru>
2024-09-03 23:29:10 +0300

    fix interface

--- 31/pkg/storage/interface.go
+++ 31/pkg/storage/interface.go
@@ -4,5 +4,7 @@ import "main.go/pkg/storage/postgres"
 
 type Interface interface {
 	Tasks(int, int) ([]postgres.Task, error)
-	//NewTask(postgres.Task) (int, error)
+	NewTask(postgres.Task) (int, error)
+	UpdateTask(t postgres.Task) error
+	DeleteTask(taskID int) error
 }
